1e5679429df46b46c946eb85ccf3a22230632814
Nircek <nircek-2103@protonmail.com>
2019-05-12 18:22:41 +0200

    Add nSock funtion bodies

--- main.c
+++ main.c
@@ -30,41 +30,114 @@ struct ntime_t {
 
 void ninit() {}
 void ncleanup() {}
-nsock nsocket() {}
-void nconnect(nsock sock, const char *name) {}
-void nsend(nsock sock, void* msg, size_t how_many) {}
-void nrecv(nsock sock, void* msg, size_t how_many) {}
-void nsendll(nsock sock, uint64_t msg) {}
-void nsendl(nsock sock, uint32_t msg) {}
-void nsends(nsock sock, uint16_t msg) {}
-void nsendc(nsock sock, uint8_t msg) {}
-void nsendnt(nsock sock, struct ntime_t msg) {}
-uint64_t nrecvll(nsock sock) {}
-uint32_t nrecvl(nsock sock) {}
-uint16_t nrecvs(nsock sock) {}
-uint8_t nrecvc(nsock sock) {}
-struct ntime_t nrecvnt(nsock sock) {}
-struct ntime_t ntime() {}
-double doublent(struct ntime_t nt) {}
-double diffdoublent(struct ntime_t nt1, struct ntime_t nt2) {}
-void nbind(nsock sock, short port) {}
-void nlisten(nsock sock) {}
-nsock naccept(nsock sock) {}
-void nclose(nsock sock) {}
-
-void client() {
-    struct hostent *he = gethostbyname("localhost");
-    unsigned short port = PORT;
-    hErr(he==NULL?-1:0, "GETHOSTBYNAME");
+nsock nsocket() {
+    return hErr(socket(AF_INET, SOCK_STREAM, 0), "nSOCKET");
+}
+void nconnect(nsock sock, const char *name, unsigned short port) {
+    struct hostent *he = gethostbyname(name);
+    hErr(he==NULL?-1:0, "nGETHOSTBYNAME");
     const char *host = inet_ntoa(**(struct in_addr**)he->h_addr_list);
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
     inet_pton(AF_INET, host, &addr.sin_addr);
-    const int sock = hErr(socket(AF_INET, SOCK_STREAM, 0), "cSOCKET");
-    hErr(connect(sock, (struct sockaddr*)&addr, sizeof(addr)), "cCONNECT");
+    hErr(connect(sock, (struct sockaddr*)&addr, sizeof(addr)), "nCONNECT");
+    
+}
+void nsendm(nsock sock, void* msg, size_t how_many) {
+    hErr(send(sock, msg, how_many, 0)-how_many, "nSEND");
+}
+void nsend(nsock sock, void* msg) {
+    nsendm(sock, msg, sizeof(msg));
+}
+void nsendll(nsock sock, uint64_t msg) {
+    nsend(sock, &msg);
+}
+void nsendl(nsock sock, uint32_t msg) {
+    nsend(sock, &msg);
+}
+void nsends(nsock sock, uint16_t msg) {
+    nsend(sock, &msg);
+}
+void nsendc(nsock sock, uint8_t msg) {
+    nsend(sock, &msg);
+}
+void nsendnt(nsock sock, struct ntime_t msg) {
+    nsend(sock, &msg.time);
+    nsend(sock, &msg.ntime);
+}
+void nrecvm(nsock sock, void* msg, size_t how_many) {
+    hErr(recv(sock, msg, how_many, 0)-how_many, "cRECV");
+}
+void nrecv(nsock sock, void* msg) {
+    nrecvm(sock, msg, sizeof(msg));
+}
+uint64_t nrecvll(nsock sock) {
+    uint64_t r;
+    nrecv(sock, &r);
+    return r;
+}
+uint32_t nrecvl(nsock sock) {
+    uint32_t r;
+    nrecv(sock, &r);
+    return r;
+}
+uint16_t nrecvs(nsock sock) {
+    uint16_t r;
+    nrecv(sock, &r);
+    return r;
+}
+uint8_t nrecvc(nsock sock) {
+    uint8_t r;
+    nrecv(sock, &r);
+    return r;
+}
+struct ntime_t nrecvnt(nsock sock) {
+    struct ntime_t r;
+    nrecv(sock, &r.time);
+    nrecv(sock, &r.ntime);
+    return r;
+}
+struct ntime_t ntime() {
+    struct timespec ts={0,0};
+    clock_gettime(CLOCK_REALTIME, &ts);
+    struct ntime_t r;
+    r.time = ts.tv_sec;
+    r.ntime = ts.tv_nsec;
+    return r;
+}
+double doublent(struct ntime_t nt) {
+    return nt.time+1e-9*nt.ntime;
+}
+double diffdoublent(struct ntime_t nt1, struct ntime_t nt2) {
+    return (nt1.time-nt2.time) + 1e-9*(nt1.ntime-nt2.ntime);
+}
+void nbind(nsock sock, short port) {
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htons( INADDR_ANY );
+    int true = 1;
+    setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,&true,sizeof(int));
+    hErr(bind(sock, (struct sockaddr*) &addr, sizeof(addr)), "nBIND");
+}
+void nlisten(nsock sock, int backlog) {
+    hErr(listen(sock, backlog), "nLISTEN");
+}
+nsock naccept(nsock sock) {
+        socklen_t len;
+        struct sockaddr_in client = {};
+        return hErr(accept(sock, (struct sockaddr*) &client, &len), "nACCEPT");
+}
+void nclose(nsock sock) {
+    hErr(shutdown(sock, SHUT_RDWR), "nSHUTDOWN");
+}
+
+void client() {
+    nsock sock = nsocket();
+    nconnect(sock, "localhost", PORT);
     char bufor[16] = "r";
-    hErr(send(sock, bufor, 1, 0)-1, "cSEND");
+    nsendc(sock, 'r');
     do {
         hErr(recv(sock, bufor, 1, 0)-1, "cRECV");
         printf("%s\n", bufor);
